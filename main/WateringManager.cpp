12f4f9aad088b9d4e88914c5c07a6722a08de6e7
aaa <bbb>
2023-06-14 10:39:32 +0800

    M

---
+++ main/WateringManager.cpp
@@ -0,0 +1,18 @@
+#include "WateringManager.h"
+
+WateringManager* pWM;
+
+void InitWateringManager(void)
+{
+
+}
+
+
+WateringManager::WateringManager(/* args */)
+{
+}
+
+WateringManager::~WateringManager()
+{
+}
+
---
+++ main/WateringManager.h
@@ -0,0 +1,23 @@
+
+
+#ifdef __cplusplus
+class WateringManager
+{
+private:
+    /* data */
+public:
+    WateringManager(/* args */);
+    ~WateringManager();
+};
+
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void InitWateringManager(void);
+
+#ifdef __cplusplus
+}
+#endif
