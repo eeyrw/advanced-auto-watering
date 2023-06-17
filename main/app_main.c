15c8f1fcab6b4ac45e36a22bde902ecf31d31ed8
yuan <ayrw@163.com>
2023-06-18 00:46:06 +0800

    M

--- main/app_main.c
+++ main/app_main.c
@@ -50,6 +50,8 @@
 #include "mqtt_credential.h"
 #include "WateringManagerWrapper.h"
 #include "TimeManger.h"
+#include "esp_littlefs.h"
+#include "esp_flash.h"
 
 static const char *TAG = "AUTO_WATERING";
 /* FreeRTOS event group to signal when we are connected & ready to make a request */
@@ -57,9 +59,6 @@ static esp_mqtt_client_handle_t mqtt_client;
 static void mqtt_app_start(void);
 
 void read_temperature_humidity_task(void *arg);
-
-
-
 
 static sht3x_t dev;
 void read_temperature_humidity_task(void *pvParameters)
@@ -287,6 +286,130 @@ static void mqtt_app_start(void)
     esp_mqtt_client_start(mqtt_client);
 }
 
+void littleFS(void)
+{
+    uint32_t size_flash_chip = 0;
+    esp_flash_get_size(NULL, &size_flash_chip);
+    printf("%uMB flash\n", (unsigned int)size_flash_chip >> 20);
+
+    printf("Free heap: %u\n", (unsigned int)esp_get_free_heap_size());
+
+    printf("Now we are starting the LittleFs Demo ...\n");
+
+    ESP_LOGI(TAG, "Initializing LittleFS");
+
+    esp_vfs_littlefs_conf_t conf = {
+        .base_path = "/littlefs",
+        .partition_label = "littlefs",
+        .format_if_mount_failed = true,
+        .dont_mount = false,
+    };
+
+    // Use settings defined above to initialize and mount LittleFS filesystem.
+    // Note: esp_vfs_littlefs_register is an all-in-one convenience function.
+    esp_err_t ret = esp_vfs_littlefs_register(&conf);
+
+    if (ret != ESP_OK)
+    {
+        if (ret == ESP_FAIL)
+        {
+            ESP_LOGE(TAG, "Failed to mount or format filesystem");
+        }
+        else if (ret == ESP_ERR_NOT_FOUND)
+        {
+            ESP_LOGE(TAG, "Failed to find LittleFS partition");
+        }
+        else
+        {
+            ESP_LOGE(TAG, "Failed to initialize LittleFS (%s)", esp_err_to_name(ret));
+        }
+        return;
+    }
+
+    size_t total = 0, used = 0;
+    ret = esp_littlefs_info(conf.partition_label, &total, &used);
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to get LittleFS partition information (%s)", esp_err_to_name(ret));
+    }
+    else
+    {
+        ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+    }
+
+    // Use POSIX and C standard library functions to work with files.
+    // First create a file.
+    ESP_LOGI(TAG, "Opening file");
+    FILE *f = fopen("/littlefs/hello.txt", "w");
+    if (f == NULL)
+    {
+        ESP_LOGE(TAG, "Failed to open file for writing");
+        return;
+    }
+    fprintf(f, "LittleFS Rocks!\n");
+    fclose(f);
+    ESP_LOGI(TAG, "File written");
+
+    // Check if destination file exists before renaming
+    struct stat st;
+    if (stat("/littlefs/foo.txt", &st) == 0)
+    {
+        // Delete it if it exists
+        unlink("/littlefs/foo.txt");
+    }
+
+    // Rename original file
+    ESP_LOGI(TAG, "Renaming file");
+    if (rename("/littlefs/hello.txt", "/littlefs/foo.txt") != 0)
+    {
+        ESP_LOGE(TAG, "Rename failed");
+        return;
+    }
+
+    // Open renamed file for reading
+    ESP_LOGI(TAG, "Reading file");
+    f = fopen("/littlefs/foo.txt", "r");
+    if (f == NULL)
+    {
+        ESP_LOGE(TAG, "Failed to open file for reading");
+        return;
+    }
+
+    char line[128];
+    char *pos;
+
+    fgets(line, sizeof(line), f);
+    fclose(f);
+    // strip newline
+    pos = strchr(line, '\n');
+    if (pos)
+    {
+        *pos = '\0';
+    }
+    ESP_LOGI(TAG, "Read from file: '%s'", line);
+
+    ESP_LOGI(TAG, "Reading from flashed filesystem example.txt");
+    f = fopen("/littlefs/example.txt", "r");
+    if (f == NULL)
+    {
+        ESP_LOGE(TAG, "Failed to open file for reading");
+        return;
+    }
+    fgets(line, sizeof(line), f);
+    fclose(f);
+    // strip newline
+    pos = strchr(line, '\n');
+    if (pos)
+    {
+        *pos = '\0';
+    }
+    ESP_LOGI(TAG, "Read from file: '%s'", line);
+
+    // All done, unmount partition and disable LittleFS
+    esp_vfs_littlefs_unregister(conf.partition_label);
+    ESP_LOGI(TAG, "LittleFS unmounted");
+}
+
 void app_main(void)
 {
     ESP_LOGI(TAG, "[APP] Startup..");
@@ -316,6 +439,7 @@ void app_main(void)
     xTaskCreate(blink_task, "blink", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
     xTaskCreate(button_init_task, "button", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
     initialise_wifi();
+    littleFS();
     struct tagWateringManager *pWM;
     pWM = GetInstance();
 
